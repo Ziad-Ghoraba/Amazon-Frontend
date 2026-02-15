55bf0fdb1073c07ab36876847dcd5596e41a4d9f
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2026-02-15 11:53:54 +0200

    Refactor checkout.js to use async/await for loading products and rendering summaries

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,12 +4,27 @@ import { loadProductsFetch } from "../data/products.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-new Promise(() => {
-    loadProductsFetch().then(() => {
-        renderOrderSummary();
-        renderPaymentSummary();
-    });
+async function loadPage() {
+    console.log("Loading page...");
+    await loadProductsFetch();
+    renderOrderSummary();
+    renderPaymentSummary();
+    return 'value2';
+}
+
+loadPage().then((value) => {
+    console.log("Page loaded.");
+    console.log(value);
 });
+
+console.log("Before loading page...");
+// async function loadPage() {
+//     return new Promise((resolve) => {
+//         console.log("Page loaded.");
+//         resolve();
+//     })
+// };
+
 
 // loadProducts(() => {
 //     renderOrderSummary();
